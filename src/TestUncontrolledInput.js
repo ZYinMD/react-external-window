b5be92ff3e87c80aced7c8affbf78df76b9e1e56
Zhi Yin <Z.Yin.MD@gmail.com>
2020-03-22 17:59:29 -0400

    turn uncontrolled input into functional component

--- src/TestUncontrolledInput.js
+++ src/TestUncontrolledInput.js
@@ -1,24 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 
-export class TestUncontrolledInput extends React.Component {
-  input = React.createRef();
-  handleSubmit = event => {
-    alert("A name was submitted: " + this.input.current.value);
+export function TestUncontrolledInput() {
+  const input = useRef(null);
+
+  function handleSubmit(event) {
+    console.log("input.current.value:\n\t", input.current.value);
     event.preventDefault();
-  };
-
-  render() {
-    return (
-      <>
-        <h1>test uncontrolled input:</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" ref={this.input} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </>
-    );
   }
+
+  return (
+    <>
+      <h1>test uncontrolled input:</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" ref={input} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
 }
