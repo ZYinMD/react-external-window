6db133ce6fb49ae441a12d885a4acc690ec8ad9b
Zhi Yin <Z.Yin.MD@gmail.com>
2020-03-24 17:07:25 -0400

    change to class components for testing

--- src/TestInput.js
+++ src/TestInput.js
@@ -1,26 +1,36 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-export function TestInput() {
-  const [value, setValue] = useState("");
-  function handleChange(event) {
+export class TestInput extends Component {
+  state = {
+    value: ""
+  };
+  handleChange = event => {
     console.log("onChange!!"); // this doesn't fire in external window
     const newValue = event.target.value;
-    setValue(newValue);
-  }
-  function handleSubmit(event) {
-    console.log("current value of input is: " + value);
+    this.setState({ value: newValue });
+  };
+  handleSubmit = event => {
+    console.log("current value of input is: " + this.state.value);
     event.preventDefault();
+  };
+
+  render() {
+    return (
+      <>
+        <h1>test input:</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            The onChange event doesn't fire in external window:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </>
+    );
   }
-  return (
-    <>
-      <h1>test input:</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          The onChange event doesn't fire in external window:
-          <input type="text" value={value} onChange={handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    </>
-  );
 }
+export default TestInput;
--- src/TestSelect.js
+++ src/TestSelect.js
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-export function TestSelect() {
-  const [value, setValue] = useState("lime");
-  function handleChange(event) {
+export class TestSelect extends Component {
+  state = {
+    value: ""
+  };
+  handleChange = event => {
     const newValue = event.target.value;
-    alert("You selected " + newValue);
-    setValue(newValue);
+    console.log("You selected " + newValue);
+    this.setState({
+      value: newValue
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>test select:</h1>
+        <form>
+          <label>
+            The onChange event doesn't fire in external window:
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value="aaa">aaa</option>
+              <option value="bbb">bbb</option>
+              <option value="ccc">ccc</option>
+            </select>
+          </label>
+        </form>
+      </>
+    );
   }
-  return (
-    <>
-      <h1>test select:</h1>
-      <form>
-        <label>
-          The onChange event doesn't fire in external window:
-          <select value={value} onChange={handleChange}>
-            <option value="aaa">aaa</option>
-            <option value="bbb">bbb</option>
-            <option value="ccc">ccc</option>
-          </select>
-        </label>
-      </form>
-    </>
-  );
 }
